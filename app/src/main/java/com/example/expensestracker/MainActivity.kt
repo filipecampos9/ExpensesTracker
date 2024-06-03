5e16db8d52b7b8b49390b1fdd34bf2a7cb4f60d3
filipecampos <fjcampos.fc@gmail.com>
2024-06-03 02:03:38 +0100

    Initial commit, base layout

---
+++ app/src/main/java/com/example/expensestracker/MainActivity.kt
@@ -0,0 +1,148 @@
+package com.example.expensestracker
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.SurfaceControl
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import com.example.expensestracker.ui.theme.ExpensesTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ExpensesTrackerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ExpensesTrackerContent()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun ExpensesTrackerTopBar(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(color = Color(0, 102, 137, 255))
+            .padding(start = 16.dp, end = 10.dp, top = 10.dp, bottom = 10.dp),
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+        )
+    }
+}
+
+@Composable
+fun PriceBox(
+    modifier: Modifier = Modifier
+) {
+    val mediumPadding = 16.dp
+    Card(
+        modifier = modifier
+            .padding(mediumPadding),
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+    ){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(mediumPadding)
+        ) {
+            Text(
+                text = ("Total Balance"),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = ("0.00 €"),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+        }
+    }
+}
+
+@Composable
+fun AddExpenseButton(modifier: Modifier = Modifier) {
+    Button(
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+            .padding(16.dp),
+    ) {
+        Text(text = "+ ADD EXPENSE")
+    }
+}
+
+@Composable
+fun ExpensesTrackerContent() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ExpensesTrackerTopBar()
+            PriceBox()
+            Spacer(modifier = Modifier.weight(1f))
+            AddExpenseButton()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ExpensesTrackerPreview() {
+    ExpensesTrackerTheme {
+        ExpensesTrackerContent()
+    }
+}
